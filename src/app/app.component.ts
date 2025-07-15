0744186cbd3f03109ce44e7401f1ba5a6216cfb2
Ocunidee <ocunidee@gmail.com>
2025-07-15 14:31:22 +0200

    Angular 20

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, model, OnInit, signal } from '@angular/core'
 import { User, UserService } from './user.service'
 import { finalize, Observable } from 'rxjs'
 import { FormsModule } from '@angular/forms'
@@ -10,30 +10,30 @@ import { FormsModule } from '@angular/forms'
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
-  user: User | undefined
-  firstName = ''
-  loading = false
+  user = signal<User | undefined>(undefined)
+  firstName = model('')
+  loading = signal(false)
 
   constructor(private userService: UserService) {}
 
   ngOnInit(): void {
     this.loadUser()
-      .subscribe(user => this.user = user)
+      .subscribe(user => this.user.set(user))
   }
 
   editUser() {
-    this.loading = true
+    this.loading.set(true)
     this.userService
-      .edit(this.firstName)
+      .edit(this.firstName())
       .pipe(/*TODO*/) // Compose the observable returned by the edit method with the one returned by loadUser()
       .subscribe(/*TODO*/) // Then subscribe to the result, save it and empty the input
   }
 
   private loadUser(): Observable<User> {
-    this.loading = true
+    this.loading.set(true)
 
     return this.userService
       .details()
-      .pipe(finalize(() => this.loading = false))
+      .pipe(finalize(() => this.loading.set(false)))
   }
 }
