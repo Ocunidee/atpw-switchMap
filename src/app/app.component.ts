03c41774f6ce854f103d4501b9aff1d15e75e44c
Ocunidee <ocunidee@gmail.com>
2025-07-15 14:36:20 +0200

    Angular 20 zoneless correction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, model, OnInit, signal } from '@angular/core'
 import { User, UserService } from './user.service'
-import { finalize, Observable } from 'rxjs'
+import { finalize, Observable, switchMap } from 'rxjs'
 import { FormsModule } from '@angular/forms'
 
 @Component({
@@ -25,8 +25,11 @@ export class AppComponent implements OnInit {
     this.loading.set(true)
     this.userService
       .edit(this.firstName())
-      .pipe(/*TODO*/) // Compose the observable returned by the edit method with the one returned by loadUser()
-      .subscribe(/*TODO*/) // Then subscribe to the result, save it and empty the input
+      .pipe(switchMap(() => this.loadUser())) // Compose the observable returned by the edit method with the one returned by loadUser()
+      .subscribe({ next: user => {
+        this.user.set(user)
+        this.firstName.set('')
+      } }) // Then subscribe to the result, save it and empty the input
   }
 
   private loadUser(): Observable<User> {
